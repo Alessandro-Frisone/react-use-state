0bdbe149639d8c61b2b91f95db7f69ff2bf82b47
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-31 16:11:58 +0100

    feat: add use State

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,13 +1,21 @@
-export default function Language({title, description}) {
+import { useState } from "react";
+
+export default function Language({ title, description }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen((current) => !current)
+  };
+
   return (
     <div className="language">
       <div className="language-title">
         <h1>{title}</h1>
-        <button className="btn">+</button>
+        <button className="btn" onClick={toggle}>
+          {isOpen ? "-" : "+"}
+        </button>
       </div>
-      <div className="language-content">
-        <p>{description}</p>
-      </div>
+      <div className="language-content">{isOpen && <p>{description}</p>}</div>
     </div>
   );
 }
