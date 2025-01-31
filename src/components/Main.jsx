0b64cd5c9e62e1fe00d81b348fc8dbef298c2bc9
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-31 14:42:29 +0100

    init project

---
+++ src/components/Main.jsx
@@ -0,0 +1,22 @@
+export default function Main() {
+  return (
+    <main>
+      <div className="container">
+        <div className="languages-list">
+          <div className="language">
+            <div className="language-title">
+              <h1>HTML</h1>
+            </div>
+            <div className="language-content">
+              <p>
+                HTML (HyperText Markup Language) è il linguaggio standard per
+                creare pagine e applicazioni web. Struttura il contenuto web e
+                fornisce elementi di base come titoli, paragrafi e immagini.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
