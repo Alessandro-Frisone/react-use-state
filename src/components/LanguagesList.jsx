c76ece88e8f5d2159da0c7c8dd0f15099aaaa61e
Alessandro-Frisone <alefri2004@gmail.com>
2025-01-31 15:26:45 +0100

    feat: add ciclo for language

---
+++ src/components/LanguagesList.jsx
@@ -0,0 +1,9 @@
+import Language from "./Language";
+
+export default function LanguagesList() {
+  return (
+    <div className="container">
+      <Language />
+    </div>
+  );
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,9 @@
+import LanguagesList from "./LanguagesList";
+
 export default function Main() {
   return (
     <main>
-      <div className="container">
-        <div className="languages-list">
-          <div className="language">
-            <div className="language-title">
-              <h1>HTML</h1>
-              <button className="btn">+</button>
-            </div>
-            <div className="language-content">
-              <p>
-                HTML (HyperText Markup Language) è il linguaggio standard per
-                creare pagine e applicazioni web. Struttura il contenuto web e
-                fornisce elementi di base come titoli, paragrafi e immagini.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+        <LanguagesList />
     </main>
   );
 }
